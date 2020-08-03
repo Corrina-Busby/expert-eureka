4d094958bb6b1f2c3434161ff9293db0e645069d
Corrina-Busby <corrinabuzzy@gmail.com>
2020-08-03 13:38:02 +1200

    learning how to extract table data for testing

--- 05.datepickerForm/index.js
+++ 05.datepickerForm/index.js
@@ -6,7 +6,11 @@ const driver = new Builder()
 
 async function datepickerForm() {
     try {
-        
+        await driver.get("https://rori4.github.io/selenium-practice/#/pages/practice/datepicker-form");
+        await (await driver).findElement(By.xpath("//input[@formcontrolname='dateOne']")).sendKeys("Sep 2, 2020");
+        await (await driver).findElement(By.xpath("//input[@formcontrolname='dateTwo']")).sendKeys("Aug 3, 2020 - Oct 1, 2020");
+        await driver.findElement(By.id("submit")).click();
+
     } catch (error) {
         console.log(error)
     }
